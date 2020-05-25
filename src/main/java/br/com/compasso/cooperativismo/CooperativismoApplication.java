7bb2c65fa88bf44cbf910c4dc7fc7119a7adc964
Nailson Nascimento de Barros <nailson.barros@pampa.compasso>
2020-05-25 10:24:18 -0300

    initial commit

---
+++ src/main/java/br/com/compasso/cooperativismo/CooperativismoApplication.java
@@ -0,0 +1,13 @@
+package br.com.compasso.cooperativismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compasso/cooperativismo/CooperativismoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compasso.cooperativismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
